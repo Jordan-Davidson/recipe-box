68ee4d7b2f9b1d628cd86672270329a7d759c7ae
Jordan Davidson <jordandavidson744@yahoo.com>
2020-05-06 10:40:37 -0400

    fixed routing issues/ things from comments

--- recipebox/urls.py
+++ recipebox/urls.py
@@ -3,6 +3,6 @@ from recipebox import views
 
 urlpatterns = [
     path('', views.index),
-    path('chef/<int:id>', views.chef),
-    path('recipe/<int:id>', views.recipe)
+    path('chef/<int:id>/', views.chef),
+    path('recipe/<int:id>/', views.recipe)
 ]
